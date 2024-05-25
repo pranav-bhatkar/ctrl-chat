297ea9167872974a995e56e1987239d5c15cfeb6
Pranav Bhatkar <68102093+pranav-bhatkar@users.noreply.github.com>
2024-05-25 17:55:00 +0530

    Refactor sidebar navigation and layout in settings page

--- apps/web/app/(app)/settings/layout.tsx
+++ apps/web/app/(app)/settings/layout.tsx
@@ -33,7 +33,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className=" space-y-6 p-10 pb-16 ">
+      <div className="space-y-6 p-10 px-4 pb-16 ">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
@@ -42,10 +42,10 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
           </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+          <div className="">{children}</div>
         </div>
       </div>
     </>
